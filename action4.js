0f8f1bb4f8cc031d9a89251b377cad12ca5d969b
erry007 <dimino.enrico@gmail.com>
2020-09-09 00:07:43 +0200

    Correzione del modificaCliente

--- action4.js
+++ action4.js
@@ -34,26 +34,37 @@ module.exports = function (app) {
   app.post("/modificaCliente", function (req, res, err) {
     var sql =
       "UPDATE gestioneaffitti.utenteCliente set gestioneAffitti.utenteCliente.cognomeC = '" +
-      req.body.cognome_iscrizioneP +
-      "' set gestioneAffitti.utenteCliente.nomeC = '" +
-      req.body.nome_iscrizioneP +
-      "' set  gestioneAffitti.utenteCliente.emailC = '" +
-      req.body.email_iscrizioneP +
-      "' set gestioneAffitti.utenteCliente.passwordC = '" +
-      req.body.password_iscrizioneP +
+      req.body.cognome_iscrizioneC +
+      "', gestioneAffitti.utenteCliente.nomeC = '" +
+      req.body.nome_iscrizioneC +
+      "', gestioneAffitti.utenteCliente.emailC = '" +
+      req.body.email_iscrizioneC +
+      "', gestioneAffitti.utenteCliente.passwordC = '" +
+      req.body.password_iscrizioneC +
       "' WHERE gestioneAffitti.utenteCliente.emailC = '" +
       req.session.emailC +
       "' ";
 
-    var sql2 =
-      "SELECT gestioneAffitti.utenteCliente.nomeC, gestioneAffitti.utenteCliente.cognomeC, gestioneAffitti.utenteCliente.emailC FROM gestioneAffitti.utenteCliente WHERE gestioneAffitti.utenteCliente.emailC = '" +
-      req.session.emailC +
-      "' ";
-
     con.query(sql, function (err, results) {
-      req.session.emailC = req.body.email_iscrizioneP;
+      console.log(results);
+      req.session.emailC = req.body.email_iscrizioneC;
+      var sql2 =
+        "SELECT gestioneAffitti.utenteCliente.nomeC, gestioneAffitti.utenteCliente.cognomeC, gestioneAffitti.utenteCliente.emailC FROM gestioneAffitti.utenteCliente WHERE gestioneAffitti.utenteCliente.emailC = '" +
+        req.session.emailC +
+        "' ";
       con.query(sql2, function (err, results) {
-        if (err) {
+        if (results.length == 1) {
+          console.log(results);
+          req.session.emailC = results[0].emailC;
+          req.session.nomeC = results[0].nomeC;
+          req.session.cognomeC = results[0].cognomeC;
+          console.log(
+            req.session.emailC + " ha effettuato l'accesso correttamente."
+          );
+          res.render("SchermataProfiloCliente.html", {
+            accessoCliente: results,
+          });
+        } else {
           res.sendFile(
             path.join(
               __dirname,
@@ -61,17 +72,6 @@ module.exports = function (app) {
               "QualcosaStorto.html"
             )
           );
-        } else {
-          console.log(results);
-          req.session.emailC = results[0].emailC;
-          req.session.nomeC = results[0].nomeC;
-          req.session.cognomeC = results[0].cognomeC;
-          console.log(
-            req.session.emailC + " ha modificato i dati correttamente."
-          );
-          res.render("SchermataProfiloClienteModificato.html", {
-            modificaCliente: results,
-          });
         }
       });
     });
